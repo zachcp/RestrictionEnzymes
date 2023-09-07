ea8977328549b6fb4332cc475083e4df62d429fe
Zachary Charlop-Powers <zcpowers@Zacharys-MacBook-Air.local>
2023-09-06 20:41:06 -0400

    use the local files

--- scripts/create_resbase.py
+++ scripts/create_resbase.py
@@ -21,14 +21,6 @@ for idx, (re_name, re_content) in enumerate(Restriction.Restriction_Dictionary.r
                     json.dump(re_content, fout)
                     
 
-# create the enum code
-#
-# // Define an enum to represent different individuals
-# enum Person {
-#     Alice,
-#     Bob,
-#     // Add more people as needed
-# }
 
 # create the files
 with open(f"data/restriction_enzymes/enum.txt", "w") as fout:
@@ -52,14 +44,15 @@ with open(f"data/restriction_enzymes/enum.txt", "w") as fout:
 #     };
 with open(f"data/restriction_enzymes/load_all_fn.txt", "w") as fout:
     fout.write("fn load_restrictionenzyme_data(enzyme: RestrictionEnzymeEnum) -> RestrictionEnzyme {\n ")
-    fout.write("    let filename = match enzyme {\n ")
+    fout.write("    let filedata = match enzyme {\n ")
     for idx, (re_name, re_content) in enumerate(Restriction.Restriction_Dictionary.rest_dict.items()):
             #if idx < 4:
                 safename = camelcase(re_name)
-                fout.write(f"       RestrictionEnzymeEnum::{safename} => \"data/restriction_enzymes/enzymedata/{safename}.json\",\n")
+                # fout.write(f"       RestrictionEnzymeEnum::{safename} => \"data/restriction_enzymes/enzymedata/{safename}.json\",\n")
+                fout.write(f"       RestrictionEnzymeEnum::{safename} => DATA_DIR.get_file(\"{safename}.json\").unwrap().contents_utf8().unwrap(),\n")
     fout.write("        };\n")
-    fout.write("    let json_data = fs::read_to_string(filename).expect(\"Failed to read JSON file\");\n")
-    fout.write("    let redata = serde_json::from_str(&json_data).expect(\"Failed to deserialize JSON data\");\n")
+    # fout.write("    let json_data = fs::read_to_string(filename).expect(\"Failed to read JSON file\");\n")
+    fout.write("    let redata = serde_json::from_str(&filedata).expect(\"Failed to deserialize JSON data\");\n")
     fout.write("    redata\n")
     fout.write("}\n")
    
